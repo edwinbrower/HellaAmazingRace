6d898355ee33e8eeb8ca32ea79cd5420282556a2
Edwin Brower <edwin.brower@gmail.com>
2017-04-01 16:42:56 -0700

    update schema with path

--- src/serv/schemas/results.js
+++ src/serv/schemas/results.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var ResultSchema = new Schema({
   title: { type: String, required: true },
   winner: { type: String, required: true },
-  time: { type: String, required: true }
+  time: { type: String, required: true },
+  path: { type: String, required: true}
 });
 
 var Results = mongoose.model('results', ResultSchema);
